3f6da3e4f482780830c1a5852b2fa985a8cb38cc
kelvin <kelvin@admir.tv>
2016-10-08 17:37:23 +0100

    Initial

---
+++ src/app/my-module/my-module.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { MyModuleComponent } from './my-module.component';
+
+describe('Component: MyModule', () => {
+  it('should create an instance', () => {
+    let component = new MyModuleComponent();
+    expect(component).toBeTruthy();
+  });
+});
